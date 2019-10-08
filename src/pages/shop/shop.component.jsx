246b04a98a53d8a1b0352d1b1b1a7bb1f66ef2aa
feelipefreitas <feelipe.freitas97@gmail.com>
2019-10-08 07:41:45 -0300

    Atual

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { Route } from 'react-router-dom';
 
+import CollectionPage from '../collection/collection.component';
 import CollectionOverview from '../../components/collections-overview/collections-overview.component';
 
-const ShopPage = ({ collections }) => (
+const ShopPage = ({ match }) => (
     <div className="shop-page">
-        <CollectionOverview />
+        <Route exact path={`${match.path}`} component={CollectionOverview} />
+        <Route exact path={`${match.path}/:collectionId`} component={CollectionPage} />
     </div>
 );
 
